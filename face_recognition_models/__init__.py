167b671ea0fbaf40f77d4927561706c1679dd203
Adam Geitgey <ageitgey@gmail.com>
2017-09-23 21:42:09 +0100

    Add 5 point model

--- face_recognition_models/__init__.py
+++ face_recognition_models/__init__.py
@@ -9,6 +9,9 @@ from pkg_resources import resource_filename
 def pose_predictor_model_location():
     return resource_filename(__name__, "models/shape_predictor_68_face_landmarks.dat")
 
+def pose_predictor_five_point_model_location():
+    return resource_filename(__name__, "models/shape_predictor_5_face_landmarks.dat")
+
 def face_recognition_model_location():
     return resource_filename(__name__, "models/dlib_face_recognition_resnet_model_v1.dat")
 
